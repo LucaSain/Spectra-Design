47d14b9f6f86c705d91391dce5dadb3c1f6f11bb
LucaSain <lucasainenco2@gmail.com>
2025-08-04 21:13:06 +0300

    ui: add projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Projects() {
   return (
     <div className="min-h-screen h-full w-screen grid grid-cols-3 gap-10 py-20 px-10">
@@ -10,8 +12,25 @@ export default function Projects() {
 
 const ProjectCard = () => {
   return (
-    <div className="h-full w-full rounded-md  bg-white ">
-      <div className="bg-red-400 w-full h-full opacity-0 transition-opacity hover:opacity-100  duration-1000 rounded-md"></div>
+    <div className="h-full w-full rounded-md relative  bg-white ">
+      <div className="relative rounded-md h-full w-full">
+        <Image
+          src="/beliefs.jpg"
+          height={1000}
+          width={1000}
+          alt=""
+          className="w-full h-full rounded-md basis-1/2 object-cover invert "
+        />
+      </div>
+      <div className=" w-full h-full opacity-0 transition-opacity hover:opacity-100 absolute inset-0 duration-1000 rounded-md">
+        <Image
+          src="/beliefs.jpg"
+          height={1000}
+          width={1000}
+          alt=""
+          className="w-full h-full rounded-md basis-1/2 object-cover "
+        />
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100   fixed z-50 shadow-2xl">
-      <a href="" className="btn btn-ghost font-semibold">
+    <div className="navbar bg-base-100 fixed z-50 shadow-2xl">
+      <Link href="/" className="btn btn-ghost font-semibold">
         Spectra Design
-      </a>
-      <a href="" className="btn btn-ghost ">
+      </Link>
+      <Link href="/projects" className="btn btn-ghost ">
         Projects
-      </a>
+      </Link>
       {/* <a href="#team" className="btn btn-ghost ">
         Team
       </a>
